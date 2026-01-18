f507e9d1151cb5b9f36bb5c7bfff842897a6bb4a
jdh <wjdehd555@gmail.com>
2026-01-18 16:06:07 +0900

    Add: api user단일 폴더에서 main폴더로 분리 & 적용 및 Main.vue 주석 한줄 제거

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -22,17 +22,4 @@ const signup = async (req) => {
   }
 }
 
-// --- Main.vue 함수 ---
-const getRecruitList = async () => {
-  try {
-    const res = await api.get('/json/recruit_list')
-    console.log(res)
-    return res
-  } catch (error) {
-    console.log('모집 리스트 로드 실패', error)
-    return error
-  }
-}
-
-
-export default { login, signup, getRecruitList }
+export default { login, signup }
